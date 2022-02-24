3456cc8ae64933f1be26521cac44ff2cdfdae9d4
Samira Arabgol <samiraarabgol@Samiras-MacBook-Pro.local>
2022-02-23 23:12:17 -0600

    add live search

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let products;
+let typingTimer;        
+let typeInterval = 500; // Half a second
 let dropDown = document.querySelector("select");
 let container = document.querySelector(".container");
+let input = document.querySelector("input");
 let selectedFilter = "";
 
 fetch('products.json')
@@ -14,10 +17,6 @@ fetch('products.json')
 
   dropDown.onchange = function(event) {
     selectedFilter = event.target.value;
-
-    while(container.firstElementChild) {
-        container.removeChild(container.firstElementChild)
-    }
 
     const filtered = products
         .filter( prod => prod.type === selectedFilter.toLocaleLowerCase());
@@ -35,19 +34,32 @@ function initialize(prods) {
         img.setAttribute("height", 200);
         div.setAttribute("class", "cell") 
         div.textContent = product.name;
-        
-        //if(product.type === selectedFilter) {
-            container.appendChild(div)
-            div.appendChild(img)
-        //}
-    
+
+        container.appendChild(div)
+        div.appendChild(img)
     })
 }
 
-function removeBoxes() {
-
+function liveSearch() {
+    let query = document.getElementsByTagName("input")[0].value;
+    console.log(query, ">>>>>")
+    let items = document.querySelectorAll('.cell');
+   
+    items.forEach(item => {
+       // console.log(item, "???????????????")
+        console.log(item, "::::::")
+        if(item.textContent.includes(query)) {
+            item.hidden = false
+        } else {
+            item.hidden = true
+        }
+    })
 }
 
+input.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+  });
 
 
 
